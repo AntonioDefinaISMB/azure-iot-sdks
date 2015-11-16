13e8139647003874c562b9246c7c27089717a608
defina_antonio <defina@ismb.it>
2015-11-16 16:44:16 +0100

    [STM32Cube] Minor changes

--- c/iothub_client/samples/iothub_client_sample_http/stm32cube/AzureIOTSDKConfigUser.h
+++ c/iothub_client/samples/iothub_client_sample_http/stm32cube/AzureIOTSDKConfigUser.h
@@ -3,7 +3,7 @@
 
 #include "AzureIOTSDKConfigDefault.h"
 
-#define AZUREIOTHUBCONNECTIONSTRING			AZUREIOTHUBCONNECTIONSTRING_INVALID
+#define AZUREIOTHUBCONNECTIONSTRING						AZUREIOTHUBCONNECTIONSTRING_INVALID				//NOTE: Unused preprocessor constant (it should be possible to modify a little bit iothub_client_sample_http.c and assigin it to connectionString) 
 
 #define DEFAULT_SSID                                    DEFAULT_SSID_INVALID                            //Wi-Fi SSID parameter to set before starting
 #define DEFAULT_SECUREKEY                               DEFAULT_SECUREKEY_INVALID                       //Wi-Fi Secure Key parameter to set before starting
